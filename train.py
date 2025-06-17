afa904b5d8ba0d31e080ba8a6d001940279e6671
steinshark <everettperson@gmail.com>
2025-06-17 05:48:15 +0000

    fix training wait

--- train.py
+++ train.py
@@ -46,6 +46,8 @@ if __name__ == "__main__":
 
     while total-free > 20_000_000_000:
         free, total = torch.cuda.mem_get_info(torch.cuda)
+        if total-free < 5_000_000_000:
+            break
         print(f"awaiting phase completion - {total-free}GB being used")
         time.sleep(300)
     print(f"commencing training")
