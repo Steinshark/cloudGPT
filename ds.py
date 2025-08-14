08a210fc6c387e2d1a56f9a95e148c2e2feca75f
steinshark <everettperson@gmail.com>
2025-08-14 08:41:33 -1000

    add all data

--- ds.py
+++ ds.py
@@ -3,11 +3,13 @@ import html
 import re
 import json 
 
+
 def strip_html(text):
     # Unescape HTML entities
     text = html.unescape(text)
     # Remove HTML tags
     return re.sub(r"<[^>]*>", "", text)
+
 
 def process_q(entry:dict):
 
@@ -16,55 +18,96 @@ def process_q(entry:dict):
     long_answers    = [strip_html(" ".join(token_list[poss['start_token']:poss['end_token']])) for poss in entry['annotations']['long_answer']]
     short_answers   = list(set([poss['text'][0] for poss in entry['annotations']['short_answers'] if poss['text']]))
     
-    if question and (len("".join(long_answers)) > 128) and len("".join(short_answers) > 4):
+    if question and (len("".join(long_answers)) > 128) and (len("".join(short_answers)) > 1):
         return (question,long_answers,short_answers)
     
     return None
-    # Extract the first long answer (if present)
-    answer = ""
-    for ann in answer_list:
-        if ann.get("long_answer", {}).get("start_token", -1) != -1:
-            answer = ann["long_answer"].get("text", "")
-            break
-    
-    # If no long answer found, fallback to short answer list
-    if not answer:
-        for ann in answer_list:
-            if "short_answers" in ann and ann["short_answers"]:
-                answer = " ".join(sa.get("text", "") for sa in ann["short_answers"])
-                break
+   
 
-    # Clean up
-    question = strip_html(question)
-    answer = strip_html(answer)
-
-    if question and answer:
-        return ((question, answer))
-    else:
-        return None
-
-def format_nq_simplified(split="train"):
+def build_nq(split="train"):
     # Load the simplified train set
-    dataset = load_dataset("natural_questions","dev",split=split,streaming=True)
-    small_dataset = []
-    
-
+    dataset         = load_dataset("natural_questions","default",split=split,streaming=True)
+    small_dataset   = []
+    saved_on        = set()
     for i, example in enumerate(dataset):
         p_ex    = process_q(example)
         if p_ex is not None:
             small_dataset.append(p_ex)
 
         #Save every 2000 samples
-        if (i+1) % 2000 == 0:
+        n_ex        = len(small_dataset) + 1 
+        if (not n_ex in saved_on) and (len(small_dataset)+1) % 2000 == 0:
             with open("finetune/nq.json",'w',encoding='utf_8') as wf:
                 wf.write(json.dumps(small_dataset))
-    
-    
+            print(f"wrote {len(small_dataset)} samples")
+            saved_on.add(n_ex)
 
-    
 
     return small_dataset
 
-if __name__ == "__main__":
-    data = format_nq_simplified("validation")
-    print(f"Sample: {data[0]}")
+def build_eli5():
+    ds = load_dataset("sentence-transformers/eli5",split='train',streaming=True)
+
+    small_dataset   = [] 
+    for i,data in enumerate(ds):    
+        small_dataset.append((data['question'],data['answer']))
+
+    with open("finetune/eli5.json",'w',encoding='utf_8') as wf:
+        wf.write(json.dumps(small_dataset))
+        print(f"wrote {len(small_dataset)} samples")
+
+def build_trivia():
+    dataset = load_dataset("sentence-transformers/trivia-qa", "pair", split="train",streaming=True)
+
+    for i,data in enumerate(dataset):
+        input(f"\n\n{data['query']}\n\n{data['answer']}")
+
+def build_dolly():
+    ds              = load_dataset("databricks/databricks-dolly-15k",split='train',streaming=True)
+    small_dataset   = [] 
+    for i,data in enumerate(ds):
+        
+        small_dataset.append((data['instruction'],data['response']))
+    
+    with open("finetune/dolly.json",'w',encoding='utf_8') as wf:
+        wf.write(json.dumps(small_dataset))
+        print(f"wrote {len(small_dataset)} samples")
+    
+def build_baize():
+    ds = load_dataset("taskydata/baize_chatbot",split='train',streaming=True)
+    small_dataset = [] 
+
+    for i,data in enumerate(ds):
+        text:str    = data['input']
+        text        = text.split('[|Human|]',maxsplit=1)[1]
+        question    = text.split('[|AI|]')[0].split()
+        answer      = text.split('[|AI|]')[1].split('[|Human|]')[0].split()
+
+        small_dataset.append((question,answer))
+
+    with open("finetune/baize.json",'w',encoding='utf_8') as wf:
+        wf.write(json.dumps(small_dataset))
+        print(f"wrote {len(small_dataset)} samples")
+
+
+
+def load_nq():
+
+    with open("finetune/nq.json",'r',encoding='utf_8') as rf:
+        small_dataset   = json.loads(rf.read())
+
+        for item in small_dataset:
+
+            q,l,s = item 
+
+            print(f"\n\n\n\n\n{q}\n\n{l}\n\n{s}")
+            input('')
+
+
+if __name__ == "__main__":  
+    #data = build_nq("train")
+    #build_dolly()
+    build_baize()
+    #build_eli5()
+    #build_trivia()
+    #load_nq()
